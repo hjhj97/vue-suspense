98dcff7a33d603a5f79aa814785da1365a2a17f6
JuHeon Ha <hjhjaueon97@gmail.com>
2024-12-15 01:44:42 +0900

    feat: suspense skeleton

---
+++ suspense-skeleton/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { defineAsyncComponent } from 'vue';
+import Skeleton from './Skeleton.vue'
+
+const AsyncComponent = defineAsyncComponent(() => import('./AsyncList.vue'))
+</script>
+
+<template>
+  <div>
+    <h2>Hello!</h2>
+    <Suspense>      
+      <AsyncComponent />      
+      <template #fallback>
+        <Skeleton />
+      </template>
+    </Suspense>
+  </div>
+</template>
---
+++ suspense-skeleton/src/Skeleton.vue
@@ -0,0 +1,41 @@
+
+<template>
+  <div class="container">
+    <ul>
+      <li v-for="i in 3" :key="i" class="item"></li>
+    </ul>
+  </div>
+</template>
+<style scoped>
+.container{
+  padding : 10px;
+  border-radius: 10px;
+  width : 100%;
+  background-color: #eaeaea;
+}
+ul{
+  display: flex;
+  flex-direction: column;
+  gap : 16px;
+}
+li{
+  width : 400px;
+  height: 30px;
+  background-color: #bcbcbc;
+  border-radius: 8px;
+  list-style-type: none;
+  animation: pulse 2s infinite;
+}
+
+@keyframes pulse{
+  0%{
+    background-color: #ccc;
+  }
+  50%{
+    background-color: #ddd;
+  }
+  100%{
+    background-color: #ccc;
+  }
+}
+</style>
