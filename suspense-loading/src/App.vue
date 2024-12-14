62c040de994feb8293964609dcbb39781255ebdf
JuHeon Ha <hjhjaueon97@gmail.com>
2024-12-15 01:45:14 +0900

    Add files via upload

---
+++ suspense-loading/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { defineAsyncComponent } from 'vue';
+
+const AsyncComponent = defineAsyncComponent(() => import('./AsyncList.vue'))
+</script>
+
+<template>
+  <div>
+    <h2>Hello!</h2>
+    <Suspense>      
+      <AsyncComponent />      
+      <template #fallback>
+        Loading...
+      </template>
+    </Suspense>
+  </div>
+</template>
---
+++ suspense-loading/src/AsyncList.vue
@@ -0,0 +1,25 @@
+<script setup>
+import {ref} from 'vue'
+
+const data = [
+{id : 0, title: 'This is first'},
+ {id : 1, title: 'This is second'},
+ {id : 2, title: 'This is third'}]
+const list = ref(null)
+
+const fetchData = async() => {
+  await new Promise(resolve => setTimeout(resolve,3000))
+  list.value = data;
+}
+await fetchData();
+
+    
+</script>
+
+<template>
+  <div class="container">
+    <ul>
+      <li v-for="item in list" :key="item.id">{{item.title}}</li>
+    </ul>
+  </div>
+</template>
